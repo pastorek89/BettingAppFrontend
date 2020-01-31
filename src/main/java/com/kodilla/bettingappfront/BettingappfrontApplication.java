c07afd63bf0c1cb3e0ef2afa34ab9f0b79445481
pastorek89 <pastor.patryk@gmail.com>
2020-01-31 08:50:22 +0100

    first commit

---
+++ src/main/java/com/kodilla/bettingappfront/BettingappfrontApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.bettingappfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingappfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettingappfrontApplication.class, args);
+    }
+
+}
